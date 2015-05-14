ab604c1a9463ff39b42d53b6ad3b5428a1387eb4
andreasmilch <coding@andreasmilch.de>
2015-05-14 22:21:23 +0200

    some files added (copied from Lars Kappert)

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+ln -sfv ${BASEDIR}/bash/.bash_profile ~/.bash_profile
 ln -sfv ${BASEDIR}/bash/.bash_prompt ~/.bash_prompt
 ln -sfv ${BASEDIR}/bash/.bashrc ~/.bashrc
+ln -sfv ${BASEDIR}/bash/.inputrc ~/.inputrc
