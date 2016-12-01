8fea8bca2893fb438db209e3e7b6347e4cf85827
Andreas Milch <mail@andreasmilch.de>
2016-12-01 12:07:36 +0100

    woop woop

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.newsbeuter/config ~/.newsbeuter/config.old 2>/dev/null
+cp ~/.newsbeuter/urls ~/.newsbeuter/urls.old 2>/dev/null
+cp ~/.elinks/elinks.conf ~/.elinks/elinks.conf.old 2>/dev/null
--- install.sh
+++ install.sh
@@ -2,14 +2,26 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# vim
 ln -sfv ${BASEDIR}/vim/.vimrc ~/.vimrc
+
+# tmux
 ln -sfv ${BASEDIR}/tmux/.tmux.conf ~/.tmux.conf
-ln -sfv ${BASEDIR}/fish/config.fish ~/.config/fish/config.fish
+
+# zsh
 ln -sfv ${BASEDIR}/zsh/.zshrc ~/.zshrc
 ln -sfv ${BASEDIR}/zsh/.aliases ~/.aliases
 ln -sfv ${BASEDIR}/zsh/.dircolors ~/.dircolors
-ln -sfv ${BASEDIR}/X/.Xresources ~/.Xresources
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
+# X11
+ln -sfv ${BASEDIR}/X/.Xresources ~/.Xresources
 xrdb -merge ~/.Xresources
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# Newsbeuter
+ln -sfv ${BASEDIR}/newsbeuter/config ~/.newsbeuter/config
+ln -sfv ${BASEDIR}/newsbeuter/urls ~/.newsbeuter/urls
+
+# elinks
+ln -sfv ${BASEDIR}/elinks/elinks.conf ~/.elinks/elinks.conf
